1e9ed40df353c4998f1c881cd69d9413dae9d5d5
michaelp <michaelp@linux-laptop.lan>
2025-01-10 19:09:23 +0000

    Added cognito user pool access

---
+++ src/xf3.js
@@ -0,0 +1,21 @@
+import { UserManager } from 'oidc-client-ts';
+
+const cognitoAuthConfig = {
+    authority: "https://cognito-idp.eu-west-2.amazonaws.com/eu-west-2_NOOFfbhA2",
+    client_id: "7eq0r136ivj5d2ihon9fm8lb2n",
+    redirect_uri: `${window.location.origin}/main.html`,
+    response_type: "code",
+    scope: "email openid phone"
+};
+
+// create a UserManager instance
+export const userManager = new UserManager({
+    ...cognitoAuthConfig,
+});
+
+export async function signOutRedirect () {
+    const clientId = "7eq0r136ivj5d2ihon9fm8lb2n";
+    const logoutUri = `${window.location.origin}/index.html`;
+    const cognitoDomain = "https://eu-west-2nooffbha2.auth.eu-west-2.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+};
