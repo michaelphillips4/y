ab45ddfbcdba01f59ca9413992ea6595b00ad25c
michaelp <michaelp@linux-laptop.lan>
2025-01-10 22:33:44 +0000

    more path fixes

--- src/xf3.js
+++ src/xf3.js
@@ -3,7 +3,7 @@ import { UserManager } from 'oidc-client-ts';
 const cognitoAuthConfig = {
     authority: "https://cognito-idp.eu-west-2.amazonaws.com/eu-west-2_NOOFfbhA2",
     client_id: "7eq0r136ivj5d2ihon9fm8lb2n",
-    redirect_uri: window.location.origin + "//main.html",
+    redirect_uri: window.location.origin + "/main.html",
     response_type: "code",
     scope: "email openid phone"
 };
@@ -15,7 +15,7 @@ export const userManager = new UserManager({
 
 export async function signOutRedirect () {
     const clientId = "7eq0r136ivj5d2ihon9fm8lb2n";
-    const logoutUri = window.location.origin + "//index.html";
+    const logoutUri = window.location.origin + "/index.html";
     const cognitoDomain = "https://eu-west-2nooffbha2.auth.eu-west-2.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 };
